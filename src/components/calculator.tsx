d9ca0eb2c6b80ad75b140ee042289ce8ba5dfa8e
KaykyOne <kaykyzioti@gmail.com>
2026-03-13 19:46:30 -0300

    asdasd

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -235,7 +235,7 @@ export default function Calculator() {
             `Valor Aproximado ${formatter.format(valorTotal)}%0A` +
             `${numeroParcelas > numeroParcelasSemJuros ? 'Com juros da maquininha inclusos!%0A%0A' : 'SEM JUROS%0A%0A'}` +
             `Por favor, me envie mais informações sobre como proceder. Obrigado!`;
-        window.open(`https://wa.me/5567981368080?text=${mensagem}`, '_blank');
+        window.open(`https://wa.me/5517997572900?text=${mensagem}`, '_blank');
     }
 
     const Buttons = () => {
